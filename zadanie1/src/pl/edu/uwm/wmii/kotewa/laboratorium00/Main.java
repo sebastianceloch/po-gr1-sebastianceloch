9530c2f24c7366a00e7985967c1caf4bd09be780
sebastianceloch <73536373+sebastianceloch@users.noreply.github.com>
2021-10-06 15:02:42 +0200

    Update Main.java

--- zadanie1/src/pl/edu/uwm/wmii/kotewa/laboratorium00/Main.java
+++ zadanie1/src/pl/edu/uwm/wmii/kotewa/laboratorium00/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println(styczen+luty+marzec);
         //zad 2
         int suma=0;
-        for(int i=0;i<=10;i++)
+        for(int i=0;i<10;i++)
         {
             suma+=i;
         }
